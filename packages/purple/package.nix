c2f6be74c497e454ab8b537780c23f7424fa92fc
John Nix <github-actions[bot]@users.noreply.github.com>
2026-09-09 03:49:22 +0000

    Flake.lock and devenv.lock update

--- packages/purple/package.nix
+++ packages/purple/package.nix
@@ -10,8 +10,8 @@
   src = fetchFromGitHub {
     owner = "erickochen";
     repo = "purple";
-    rev = "7f4764f2157baef79e0bd5d2af14cd246cede115";
-    hash = "sha256-o9RH2bl1zxNnOD2wE+Xwb2godWQfVnlxMJ33V2zmaP4=";
+    rev = "cf7b25b27ddd0185a41330e10c8c68917fae60bb";
+    hash = "sha256-tVAWzZFDlroNm1yUAPi620R9FPoOfIo4aZhm6DuTsSo=";
   };
 
   cargoToml = lib.importTOML "${src.outPath}/Cargo.toml";
